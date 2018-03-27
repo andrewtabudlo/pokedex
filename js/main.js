4f47d7f81260dd81235a10047f752d9b24dfc53b
Andrew Tabudlo <andrew.tabudlo@gmail.com>
2018-03-27 04:51:30 -0400

    change pokemon, add misc tab, fix grid

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-class Pokedex {
+class Trainer {
     constructor(pokemon) {
-        this.pokemon = [];
+      this.pokemon = [];
     }
 
     addPokemon(x) {
-            this.pokemon.push(x);
+      this.pokemon.push(x);
         }
+    all() {
+      return this.pokemon;
+    }
+    get(key) {
+      for (let i = 0; i < this.pokemon.length; i++) {
+        if (this.pokemon[i].name === key) {
+          return this.pokemon[i];
+        }
+      }
+    }
 }
 
 class Pokemon {
@@ -24,11 +34,13 @@ class Pokemon {
     }
 };
 
-let pokedex = new Pokedex();
+let daenerys = new Trainer();
 
-let getArticuno = function(){
+//create li for each
+
+let getSalamence = function(){
     $.ajax({
-    url: 'http://pokeapi.salestock.net/api/v2/pokemon/144/',
+    url: 'https://pokeapi.co/api/v2/pokemon/373/',
     dataType: 'json',
     method: 'GET',
     success: function(data) {
@@ -48,14 +60,13 @@ let getArticuno = function(){
         }
         getAbilities();
         let articuno = new Pokemon(data.id,data.name,data.sprites.front_default,types,data.weight,data.height,data.stats[3].base_stat,data.stats[4].base_stat,data.stats[5].base_stat,abilities);
-        pokedex.addPokemon(articuno);
-        $('#pokemonOnePic').attr('src',pokedex.pokemon[0].image);
+        daenerys.addPokemon(articuno);
     }
 })};
 
-let getMoltres = function(){
+let getRayquaza = function(){
     $.ajax({
-    url: 'http://pokeapi.salestock.net/api/v2/pokemon/146/',
+    url: 'https://pokeapi.co/api/v2/pokemon/384/',
     dataType: 'json',
     method: 'GET',
     success: function(data) {
@@ -75,13 +86,13 @@ let getMoltres = function(){
         }
         getAbilities();
         let moltres = new Pokemon(data.id,data.name,data.sprites.front_default,types,data.weight,data.height,data.stats[3].base_stat,data.stats[4].base_stat,data.stats[5].base_stat,abilities);
-        pokedex.addPokemon(moltres);
+        daenerys.addPokemon(moltres);
     }
 })};
 
 let getDragonite = function(){
     $.ajax({
-    url: 'http://pokeapi.salestock.net/api/v2/pokemon/149/',
+    url: 'https://pokeapi.co/api/v2/pokemon/149/',
     dataType: 'json',
     method: 'GET',
     success: function(data) {
@@ -101,31 +112,55 @@ let getDragonite = function(){
         }
         getAbilities();
         let dragonite = new Pokemon(data.id,data.name,data.sprites.front_default,types,data.weight,data.height,data.stats[3].base_stat,data.stats[4].base_stat,data.stats[5].base_stat,abilities);
-        pokedex.addPokemon(dragonite);
+        daenerys.addPokemon(dragonite);
     },
     complete: function() {
+      setTimeout(function(){
         $('#kantoButton').addClass('pulse').removeClass('disabled');
-        $('#pokemonPic1').attr('src',pokedex.pokemon[0].image);
-        $('#pokemonPic2').attr('src',pokedex.pokemon[1].image);
-        $('#pokemonPic3').attr('src',pokedex.pokemon[2].image);
+        $('#pokemonPic1').attr('src',daenerys.pokemon[0].image);
+        $('#pokemonPic2').attr('src',daenerys.pokemon[1].image);
+        $('#pokemonPic3').attr('src',daenerys.pokemon[2].image);
+        $('#cardTitle1').html('#' + daenerys.pokemon[0].id + ' ' + daenerys.pokemon[0].name);
+        $('#cardTitle2').html('#' + daenerys.pokemon[1].id + ' ' + daenerys.pokemon[1].name);
+        $('#cardTitle3').html('#' + daenerys.pokemon[2].id + ' ' + daenerys.pokemon[2].name);
+        $('#hp1').html('HP: ' + daenerys.pokemon[0].hp + '/' + daenerys.pokemon[0].hp);
+        $('#hp2').html('HP: ' + daenerys.pokemon[1].hp + '/' + daenerys.pokemon[1].hp);
+        $('#hp3').html('HP: ' + daenerys.pokemon[2].hp + '/' + daenerys.pokemon[2].hp);
+        $('#attack1').html('Attack: ' + daenerys.pokemon[0].attack);
+        $('#attack2').html('Attack: ' + daenerys.pokemon[1].attack);
+        $('#attack3').html('Attack: ' + daenerys.pokemon[2].attack);
+        $('#defense1').html('Defense: ' + daenerys.pokemon[0].defense);
+        $('#defense2').html('Defense: ' + daenerys.pokemon[1].defense);
+        $('#defense3').html('Defense: ' + daenerys.pokemon[2].defense);
+        $('#height1').html('Height: ' + daenerys.pokemon[0].height);
+        $('#height2').html('Height: ' + daenerys.pokemon[1].height);
+        $('#height3').html('Height: ' + daenerys.pokemon[2].height);
+        $('#weight1').html('Weight: ' + daenerys.pokemon[0].weight);
+        $('#weight2').html('Weight: ' + daenerys.pokemon[1].weight);
+        $('#weight3').html('Weight: ' + daenerys.pokemon[2].weight);
+        daenerys.pokemon[0].abilities.forEach(function(ability){
+          $('#abilitiesList1').append(`<li class="list-group-item">${ability}</li>`);
+        });
+        daenerys.pokemon[1].abilities.forEach(function(ability){
+          $('#abilitiesList2').append(`<li class="list-group-item">${ability}</li>`);
+        });
+        daenerys.pokemon[2].abilities.forEach(function(ability){
+          $('#abilitiesList3').append(`<li class="list-group-item">${ability}</li>`);
+        });
+      },5000);
+      }
     }
-})};
+)};
 
-getArticuno();
-getMoltres();
+getSalamence();
+getRayquaza();
 getDragonite();
 
-console.log(pokedex);
-
-let createMenus = function() {
-    for (let i = 0; i < pokedex.length; i++){
-        $('<img></img>').html(pokemon[i].image).attr('class','pokeImage');
-
-    }
-};
+console.log(daenerys);
 
 $('#kantoButton').click(function(){
     event.preventDefault();
-    $('#systemMessage').addClass('invisible');
+    $('#systemMessage').addClass('d-none');
     $('#menuScreen').addClass('visible').removeClass('invisible');
+    $('#bootScroll').addClass('d-none');
 });
